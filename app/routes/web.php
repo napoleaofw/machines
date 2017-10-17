a14c737116ccb70682a5533e4fce25fb5d08194c
Napoleão Fusiger Wingert <napoleao@kunden.local>
2017-10-17 17:12:03 -0200

    refactoring++

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login', function () {
+    return view('login.index');
+});
+
+Route::get('/user/register', function () {
+    return view('user.form.index');
+});
+
+Route::get('/user/list', function () {
+    return view('user.list.index');
+});
+
+Route::get('/establishment/register', function () {
+    return view('establishment.form.index');
+});
+
+Route::get('/establishment/list', function () {
+    return view('establishment.list.index');
+});
+
+Route::get('/machine/register', function () {
+    return view('machine.form.index');
+});
+
+Route::get('/machine/list', function () {
+    return view('machine.list.index');
+});
+
+Route::get('/transaction/register', function () {
+    return view('transaction.register.index');
+});
+
+Route::get('/transaction/list', function () {
+    return view('transaction.list.index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
 });
