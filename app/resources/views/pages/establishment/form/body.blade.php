3f986d08753ece06680542b0c7bbf416891b49ba
Napoleão Fusiger Wingert <napoleao@kunden.local>
2017-11-21 17:12:54 -0200

    transaction

--- app/resources/views/pages/establishment/form/body.blade.php
+++ app/resources/views/pages/establishment/form/body.blade.php
@@ -7,11 +7,56 @@
                 <h4 class="modal-title">Dados da transação</h4>
             </div>
             <div class="modal-body">
-                <div class="row">
-                    <div class="col-xs-12">
-                        campos do cadastro
+                <form role="form">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Estabelecimento</label>
+                                <select class="form-control">
+                                    <option value="">Selecione</option>
+                                    <option value="estabelecimento1">Estabelecimento 1</option>
+                                    <option value="estabelecimento2">Estabelecimento 2</option>
+                                    <option value="estabelecimento3">Estabelecimento 3</option>
+                                    <option value="estabelecimento4">Estabelecimento 4</option>
+                                    <option value="estabelecimento5">Estabelecimento 5</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Máquina</label>
+                                <select class="form-control">
+                                    <option value="">Selecione</option>
+                                    <option value="maquina1">Máquina 1</option>
+                                    <option value="maquina2">Máquina 2</option>
+                                    <option value="maquina3">Máquina 3</option>
+                                    <option value="maquina4">Máquina 4</option>
+                                    <option value="maquina5">Máquina 5</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Observação</label>
+                                <textarea class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Tipo</label>
+                                <select class="form-control">
+                                    <option value="">Selecione</option>
+                                    <option value="despesa">Despesa</option>
+                                    <option value="receita">Receita</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Quantidade de créditos</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Data</label>
+                                <input type="text" class="form-control input-date">
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cancelar</button>
