d337c81d9be3daa7a71f0af28c4c5e4aa3b41bf6
napoleaofw <napoleao.wingert@gmail.com>
2017-11-27 19:27:33 -0200

    views and css

--- app/resources/views/reports/establishment/body.blade.php
+++ app/resources/views/reports/establishment/body.blade.php
@@ -7,60 +7,77 @@
                 Relatório resumido por estabelecimento
             </h1>
         </section>
-        <section class="report">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h2 class="page-header">
-                        <i class="fa fa-globe"></i> Nome da empresa
-                        <small class="pull-right">Data: 18/10/2017</small>
-                    </h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Data</th>
-                            </tr>
-                            <tr>
-                                <th>Estabelecimento</th>
-                            </tr>
-                            <tr>
-                                <th>Usuário</th>
-                                <th>Máquina</th>
-                                <th>Valor do crédito</th>
-                                <th>Tipo</th>
-                                <th>Valor de comissão</th>
-                                <th>Valor total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($recordsReport as $date => $recordsEstablishment)
-                        <tr>
-                            <th>Data: {{ $date ?: 'em aberto' }}</th>
-                        </tr>
-                            @foreach($recordsEstablishment as $establishment => $recordsTransaction)
-                                <tr>
-                                    <th>Estabelecimento: {{ $establishment }}</th>
-                                </tr>
-                                @foreach($recordsTransaction as $recordTransaction)
+        <section class="content">
+        <!-- <section class="report"> -->
+            <div class="box box-widget">
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2 class="page-header">
+                                <i class="fa fa-globe"></i> Nome da empresa
+                                <small class="pull-right">Data: 18/10/2017</small>
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
                                     <tr>
-                                        <td>{{ $recordTransaction->createdBy->name }}</td>
-                                        <td>{{ $recordTransaction->machine->code . ' - ' . $recordTransaction->machine->name }}</td>
-                                        <td>R$ {{ $recordTransaction->credit_value }}</td>
-                                        <td>{{ $recordTransaction->type == 'receive' ? 'Receita' : 'Despesa' }}</td>
-                                        <td>R$ {{ $recordTransaction->commission_value }}</td>
-                                        <td>R$ {{ $recordTransaction->credit_amount }}</td>
+                                        <th>Usuário</th>
+                                        <th>Máquina</th>
+                                        <th class="text-right">Valor do crédito</th>
+                                        <th>Tipo de transação</th>
+                                        <th class="text-right">Valor de comissão</th>
+                                        <th class="text-right">Valor total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($recordsReport as $date => $recordsEstablishment)
+                                <tr>
+                                    <th colspan="6">Data: {{ $date ?: 'em aberto' }}</th>
+                                </tr>
+                                    @foreach($recordsEstablishment as $establishment => $recordsTransaction)
+                                        <tr>
+                                            <th colspan="6">Estabelecimento: {{ $establishment }}</th>
+                                        </tr>
+                                        @foreach($recordsTransaction as $recordTransaction)
+                                            <tr>
+                                                <td>{{ $recordTransaction->createdBy->name }}</td>
+                                                <td>{{ $recordTransaction->machine->code . ' - ' . $recordTransaction->machine->name }}</td>
+                                                <td class="text-right">R$ {{ $recordTransaction->credit_value }}</td>
+                                                <td>{{ $recordTransaction->type == 'receive' ? 'Receita' : 'Despesa' }}</td>
+                                                <td class="text-right">R$ {{ $recordTransaction->commission_value }}</td>
+                                                <td class="text-right">R$ {{ $recordTransaction->credit_amount }}</td>
+                                            </tr>
+                                        @endforeach
+                                        <tr>
+                                            <th class="text-right" colspan="2">Total estabelecimento {{ $establishment }}:</th>
+                                            <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                            <th></th>
+                                            <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                            <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                        </tr>
+                                    @endforeach
+                                    <tr>
+                                        <th class="text-right" colspan="2">Total data {{ $date }}:</th>
+                                        <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                        <th></th>
+                                        <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                        <th class="text-right">R$ {{ 'XXXXX' }}</th>
                                     </tr>
                                 @endforeach
-                            @endforeach
-                        @endforeach
-                            <tr>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                <tr>
+                                    <th class="text-right" colspan="2">Total:</th>
+                                    <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                    <th></th>
+                                    <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                    <th class="text-right">R$ {{ 'XXXXX' }}</th>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- app/resources/views/reports/invoice/body.blade.php
+++ app/resources/views/reports/invoice/body.blade.php
@@ -8,138 +8,143 @@
                 <small>#000000001</small>
             </h1>
         </section>
-        <section class="invoice">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h2 class="page-header">
-                        <i class="fa fa-globe"></i> Nome da empresa
-                        <small class="pull-right">Data: 18/10/2017</small>
-                    </h2>
-                </div>
-            </div>
-            <div class="row invoice-info">
-                <div class="col-sm-4 invoice-col">
-                    De
-                    <address>
-                        <strong>Nome da empresa</strong><br>
-                        Rua da empresa, 100 - Centro<br>
-                        Porto Alegre - RS, 00000-000<br>
-                        Telefone: (51) 99999-9999<br>
-                        E-mail: empresa@empresa.com
-                    </address>
-                </div>
-                <div class="col-sm-4 invoice-col">
-                    Para
-                    <address>
-                        <strong>Estabelecimento</strong><br>
-                        Rua do estabelecimento, 200 - Centro<br>
-                        São Paulo - SP, 00000-000<br>
-                        Telefone: (11) 98888-8888<br>
-                        E-mail: estabelecimento@00000-000.com
-                    </address>
-                </div>
-                <div class="col-sm-4 invoice-col">
-                    <b>Invoice #000000001</b><br>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Data</th>
-                                <th>Máquina</th>
-                                <th>Observação</th>
-                                <th>Saldo</th>
-                                <th>Comissão</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>12/09/2017</td>
-                                <td>Máquina 2</td>
-                                <td></td>
-                                <td>R$ 285,34</td>
-                                <td>R$ 28,53</td>
-                            </tr>
-                            <tr>
-                                <td>18/09/2017</td>
-                                <td>Máquina 1</td>
-                                <td>A máquina apresentou problema durante o período da tarde.</td>
-                                <td>R$ 50,86</td>
-                                <td>R$ 5,09</td>
-                            </tr>
-                            <tr>
-                                <td>25/09/2017</td>
-                                <td>Máquina 3</td>
-                                <td></td>
-                                <td>R$ 98,63</td>
-                                <td>R$ 9,86</td>
-                            </tr>
-                            <tr>
-                                <td>27/09/2017</td>
-                                <td>Máquina 2</td>
-                                <td></td>
-                                <td>R$ 154,25</td>
-                                <td>R$ 15,43</td>
-                            </tr>
-                            <tr>
-                                <td>29/09/2017</td>
-                                <td>Máquina 1</td>
-                                <td></td>
-                                <td>R$ 74,58</td>
-                                <td>R$ 7,46</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="row">
-                <!-- <div class="col-sm-6 col-xs-12">
-                    <p class="lead">Comissão por máquina</p>
-                    <div class="text-muted well well-sm no-shadow">
-                        <div class="table-responsive table-machines">
-                            <table class="table">
-                                <tr>
-                                    <th>Máquina 1:</th>
-                                    <td>R$ 12,55</td>
-                                </tr>
-                                <tr>
-                                    <th>Máquina 2:</th>
-                                    <td>R$ 43,96</td>
-                                </tr>
-                                <tr>
-                                    <th>Máquina 3:</th>
-                                    <td>R$ 9,86</td>
-                                </tr>
+        <section class="content">
+        <!-- <section class="invoice"> -->
+            <div class="box box-widget">
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h2 class="page-header">
+                                <i class="fa fa-globe"></i> Nome da empresa
+                                <small class="pull-right">Data: 18/10/2017</small>
+                            </h2>
+                        </div>
+                    </div>
+                    <div class="row invoice-info">
+                        <div class="col-sm-4 invoice-col">
+                            De
+                            <address>
+                                <strong>Nome da empresa</strong><br>
+                                Rua da empresa, 100 - Centro<br>
+                                Porto Alegre - RS, 00000-000<br>
+                                Telefone: (51) 99999-9999<br>
+                                E-mail: empresa@empresa.com
+                            </address>
+                        </div>
+                        <div class="col-sm-4 invoice-col">
+                            Para
+                            <address>
+                                <strong>Estabelecimento</strong><br>
+                                Rua do estabelecimento, 200 - Centro<br>
+                                São Paulo - SP, 00000-000<br>
+                                Telefone: (11) 98888-8888<br>
+                                E-mail: estabelecimento@00000-000.com
+                            </address>
+                        </div>
+                        <div class="col-sm-4 invoice-col">
+                            <b>Invoice #000000001</b><br>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12 table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Data</th>
+                                        <th>Máquina</th>
+                                        <th>Observação</th>
+                                        <th>Saldo</th>
+                                        <th>Comissão</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>12/09/2017</td>
+                                        <td>Máquina 2</td>
+                                        <td></td>
+                                        <td>R$ 285,34</td>
+                                        <td>R$ 28,53</td>
+                                    </tr>
+                                    <tr>
+                                        <td>18/09/2017</td>
+                                        <td>Máquina 1</td>
+                                        <td>A máquina apresentou problema durante o período da tarde.</td>
+                                        <td>R$ 50,86</td>
+                                        <td>R$ 5,09</td>
+                                    </tr>
+                                    <tr>
+                                        <td>25/09/2017</td>
+                                        <td>Máquina 3</td>
+                                        <td></td>
+                                        <td>R$ 98,63</td>
+                                        <td>R$ 9,86</td>
+                                    </tr>
+                                    <tr>
+                                        <td>27/09/2017</td>
+                                        <td>Máquina 2</td>
+                                        <td></td>
+                                        <td>R$ 154,25</td>
+                                        <td>R$ 15,43</td>
+                                    </tr>
+                                    <tr>
+                                        <td>29/09/2017</td>
+                                        <td>Máquina 1</td>
+                                        <td></td>
+                                        <td>R$ 74,58</td>
+                                        <td>R$ 7,46</td>
+                                    </tr>
+                                </tbody>
                             </table>
                         </div>
                     </div>
-                </div> -->
-                <div class="col-sm-6 col-sm-offset-6 col-xs-12">
-                    <p class="lead">Totais</p>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tr>
-                                <th style="width:50%">Saldo:</th>
-                                <td>R$ 561,94</td>
-                            </tr>
-                            <tr>
-                                <th>Comissão:</th>
-                                <td>R$ 56,19</td>
-                            </tr>
-                            <tr>
-                                <th>Total:</th>
-                                <td>R$ 505,75</td>
-                            </tr>
-                        </table>
+                    <div class="row">
+                        <!-- <div class="col-sm-6 col-xs-12">
+                            <p class="lead">Comissão por máquina</p>
+                            <div class="text-muted well well-sm no-shadow">
+                                <div class="table-responsive table-machines">
+                                    <table class="table">
+                                        <tr>
+                                            <th>Máquina 1:</th>
+                                            <td>R$ 12,55</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Máquina 2:</th>
+                                            <td>R$ 43,96</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Máquina 3:</th>
+                                            <td>R$ 9,86</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div> -->
+                        <div class="col-sm-6 col-sm-offset-6 col-xs-12">
+                            <p class="lead">Totais</p>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tr>
+                                        <th style="width:50%">Saldo:</th>
+                                        <td>R$ 561,94</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Comissão:</th>
+                                        <td>R$ 56,19</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Total:</th>
+                                        <td>R$ 505,75</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="row no-print">
-                <div class="col-xs-12">
-                    <button type="button" class="btn btn-default btn-print"><i class="fa fa-print"></i> Imprimir</a>
-                    <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Finalizar</button>
+                    <div class="row no-print">
+                        <div class="col-xs-12">
+                            <button type="button" class="btn btn-default btn-print"><i class="fa fa-print"></i> Imprimir</a>
+                            <button type="button" class="btn btn-success pull-right"><i class="fa fa-credit-card"></i> Finalizar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
